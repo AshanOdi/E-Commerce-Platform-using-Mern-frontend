c8ec4c6a767e3e830768d3cbced8dc52d4eb0abe
AshanOdi <ashanuni25@gmail.com>
2025-09-09 14:53:13 +0530

    feat: add HomePage, LoginPage, and SignUpPage components with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
 import Header from "./components/header";
 import ProductCard from "./components/productCard";
+import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
+import SignUpPage from "./pages/signup";
 
 function App() {
   return (
     <div className="w-full h-screen bg-red-100 flex flex-col items-center justify-center">
-      <div className="relative w-[600px] h-[600px]  bg-green-500 flex flex-col items-center justify-center">
+      {/* <div className="relative w-[600px] h-[600px]  bg-green-500 flex flex-col items-center justify-center">
         <div className="w-[100px] h-[100px] bg-blue-600"></div>
         <div className="w-[100px] h-[100px] bg-green-600"></div>
         <div className="w-[100px] h-[100px] bg-yellow-600 absolute top-[10px] left-[10px]"></div>
         <div className="w-[100px] h-[100px] bg-purple-600"></div>
         <div className="w-[100px] h-[100px] bg-pink-600"></div>
-      </div>
+      </div> */}
+
+      <div>{<HomePage />}</div>
     </div>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+export default function HomePage() {
+  return (
+    <div class="max-w-3xl mx-auto p-8 bg-white shadow-lg rounded-lg text-center">
+      <h1 class="text-4xl font-bold text-gray-800 mb-4">
+        Crystal Beauty Clear
+      </h1>
+      <p class="text-lg text-gray-600 mb-6 leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
+        quibusdam corrupti tempore maxime sit voluptatum provident sequi
+        quisquam nobis optio voluptate delectus quae, facere sunt saepe earum
+        perferendis ipsa! Et exercitationem atque error delectus, doloribus
+        alias officiis quisquam perspiciatis vel velit, cum ducimus accusantium
+        amet optio laudantium neque, quaerat repudiandae.
+      </p>
+      <button class="px-6 py-3 bg-indigo-600 hover:bg-indigo-700 text-white font-medium rounded-md shadow-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50">
+        Home
+      </button>
+    </div>
+  );
+}
