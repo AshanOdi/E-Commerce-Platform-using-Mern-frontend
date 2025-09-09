eaae821590e3dbb519f500bddabbcdfa1ff0d0bd
AshanOdi <ashanuni25@gmail.com>
2025-09-09 14:39:56 +0530

    refactor: remove Header and ProductCard components from App

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import ProductCard from "./components/productCard";
 
 function App() {
   return (
-    <>
-      <Header name="ashan" price="1000" description="yoyo" />
-
-      <ProductCard></ProductCard>
-    </>
+    <div className="w-full h-screen bg-red-100 flex flex-col items-center justify-center">
+      <div className="relative w-[600px] h-[600px]  bg-green-500 flex flex-col items-center justify-center">
+        <div className="w-[100px] h-[100px] bg-blue-600"></div>
+        <div className="w-[100px] h-[100px] bg-green-600"></div>
+        <div className="w-[100px] h-[100px] bg-yellow-600 absolute top-[10px] left-[10px]"></div>
+        <div className="w-[100px] h-[100px] bg-purple-600"></div>
+        <div className="w-[100px] h-[100px] bg-pink-600"></div>
+      </div>
+    </div>
   );
 }
 
