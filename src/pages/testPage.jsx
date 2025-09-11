976ec7157c50e580f9bd3fbb0957c93a3dea67cb
AshanOdi <ashanuni25@gmail.com>
2025-09-11 21:37:16 +0530

    fileUpload function to supabase (v1)

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,49 +1,52 @@
+import { createClient } from "@supabase/supabase-js";
 import { useState } from "react";
 
 export default function TestPage() {
-  const [count, setCount] = useState(0);
-  const [value, setValue] = useState("Failed");
+  const [image, setImage] = useState(null);
+
+  const url = "https://yqizeshpdzuerwuxsyul.supabase.co";
+  const key =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxaXplc2hwZHp1ZXJ3dXhzeXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MDM0NzgsImV4cCI6MjA3MzE3OTQ3OH0.kCKEakunFS_Sc3imE2WfhqeRB8jNAYuGZDKIEtVwEJA";
+
+  const supabase = createClient(url, key);
+
+  function buttonClick() {
+    //meken supabase ekata yawannona image eka
+    //dewal 2k dennona.danna one namai file ekai
+    // supabase.storage.from("images").upload("test.jpn" ,  image)
+    supabase.storage
+      .from("images")
+      .upload(image.name, image, {
+        upsert: false,
+        cacheControl: "3600",
+      })
+      .then(() => {
+        const publicUrl = supabase.storage
+          .from("images")
+          .getPublicUrl(image.name).data.publicUrl;
+        console.log(publicUrl);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
   return (
     <div className="w-full h-screen  flex justify-center items-center flex-col">
-      <div className="w-[450px] h-[250px] bg-green-400 flex justify-center items-center">
-        <button
-          onClick={() => {
-            setCount(count - 1);
-          }}
-          className="bg-blue-600 text-white font-bold text-center w-[100px] h-[100px] text-[20px] mx-[20px] cursor-pointer"
-        >
-          -
-        </button>
-        <span>{count}</span>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-          className="bg-blue-600 text-white font-bold text-center w-[100px] h-[100px] text-[20px] mx-[20px] cursor-pointer"
-        >
-          +
-        </button>
-      </div>
-      <div className="w-[450px] h-[250px] bg-blue-400 flex justify-center items-center">
-        <button
-          className="w-[100px] h-[100px] bg-red-400 flex justify-center items-center"
-          onClick={() => {
-            setValue("Fail");
-          }}
-        >
-          Fail
-        </button>
-        <span>{value}</span>
-        <button
-          className="w-[100px] h-[100px] bg-red-400 flex justify-center items-center"
-          onClick={() => {
-            setValue("Pass");
-          }}
-        >
-          Pass
-        </button>
-      </div>
+      <input
+        onChange={(e) => {
+          setImage(e.target.files[0]);
+          // console.log(e.target.files[0]);
+        }}
+        type="file"
+        className="file-input file-input-bordered w-full max-w-xs"
+      />
+      <button
+        onClick={buttonClick}
+        className="bg-green-500 text-white font-bold py-2 px-4 rounded"
+      >
+        Upload
+      </button>
     </div>
   );
 }
